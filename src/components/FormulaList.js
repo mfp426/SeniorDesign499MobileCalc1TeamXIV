44a519d6fc4b69f28044bcf56401a8bf251ee6e9
benwoosley <benjaminwoosley@gmail.com>
2023-11-01 16:03:32 -0400

    decrease number of button rows on larger screens

--- src/components/FormulaList.js
+++ src/components/FormulaList.js
@@ -9,11 +9,11 @@ const FormulaList = ({ categoryName, buttons }) => {
         <div className="container mb-5 center">
             <div className="box justify-content-center">
                 <Header text={categoryName}/>
-                <div className="grid grid-cols-1 md:grid-cols-4 lg:grid-cols-6 gap-4">
+                <div className="grid grid-cols-1 md:grid-cols-4 lg:grid-cols-4 gap-4">
                     {buttons.map((button, index) => (
                         <button
                             key={index}
-                            className="btn btn-primary mt-4 text-truncate" // Added text-truncate class
+                            className="btn btn-primary mt-4 text-truncate"
                             onClick={() => navigate(button.route)}
                         >
                             {button.title}
