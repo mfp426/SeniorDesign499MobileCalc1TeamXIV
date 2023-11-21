31d06006365df6e8d96bca099f419eb99e30459d
apokharel1 <98554764+apokharel1@users.noreply.github.com>
2023-11-21 00:40:13 -0500

    Create KInEnergyAndWorkPage.jsx

---
+++ src/pages/CategoryPages/KinEnergyAndWork/KInEnergyAndWorkPage.jsx
@@ -0,0 +1,17 @@
+import ButtonGrid from "../../../components/ButtonGrid";
+
+
+function KinEnergyAndWork() {
+
+    const navigationButtons = [
+        { route: "/KinEnergyCalPage", title: "Kinetic Energy Calculation" },
+        { route: "/KinEnergyAndWork/WorkCalPage", title: "Work Calculation" },
+        { route: "/Conversions/SpeedAndVelocity", title: "Speed / Velocity based on Kinetic Energy" },
+    ];
+
+    return(
+        <ButtonGrid categoryName={"Kinetic Energy and Work"} buttons={navigationButtons} />
+    )
+}
+
+export default KinEnergyAndWork;
