75f7d62cab23eadb3739aca366bbdd023ad8c8c6
benwoosley <benjaminwoosley@gmail.com>
2023-11-21 01:06:40 -0500

    add a few basic test, and set up project for testing via vitest

---
+++ src/tests/App.test.jsx
@@ -0,0 +1,14 @@
+import App from "../App.jsx";
+import { render, screen } from "@testing-library/react";
+import React from "react";
+
+it('App displays the correct title', () => {
+    render(<App />);
+
+    // Use the screen utility to query for the h1 element
+    const titleElement = screen.getByText('Calculator');
+
+    // Now you can make your assertions
+    expect(titleElement).toBeInTheDocument();
+
+});
