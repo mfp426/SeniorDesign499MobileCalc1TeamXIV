c7b68775c406d15f5962ecbaafe8d4d25ff7e2c5
benwoosley <benjaminwoosley@gmail.com>
2023-11-21 12:29:51 -0500

    add testing for FormulaManagement and UI of Components

--- src/tests/Formula.test.jsx
+++ src/tests/Formula.test.jsx
@@ -1,24 +1,24 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen } from "@testing-library/react";
 import Formula from "../components/Formula.jsx";
 
-describe('Formula component tests', () => {
-    it('renders Formula component with default values', () => {
+describe("Formula component tests", () => {
+    it("renders Formula component with default values", () => {
         render(<Formula />);
-        expect(screen.getByText('Formula')).toBeInTheDocument();
-        expect(screen.getByText('Calculate')).toBeInTheDocument();
+        expect(screen.getByText("Formula")).toBeInTheDocument();
+        expect(screen.getByText("Calculate")).toBeInTheDocument();
     });
 
-    it('renders Formula component with custom values', () => {
-        const formulaName = 'Custom Formula';
-        const numericFields = ['Field 1', 'Field 2'];
-        const toggleFields = ['Toggle 1', 'Toggle 2'];
+    it("renders Formula component with custom values", () => {
+        const formulaName = "Custom Formula";
+        const numericFields = ["Field 1", "Field 2"];
+        const toggleFields = ["Toggle 1", "Toggle 2"];
 
         render(
             <Formula
                 formulaName={formulaName}
                 numericFields={numericFields}
                 toggleFields={toggleFields}
-            />
+            />,
         );
 
         expect(screen.getByText(formulaName)).toBeInTheDocument();
@@ -31,6 +31,6 @@ describe('Formula component tests', () => {
             expect(screen.getByText(field)).toBeInTheDocument();
         });
 
-        expect(screen.getByText('Calculate')).toBeInTheDocument();
+        expect(screen.getByText("Calculate")).toBeInTheDocument();
     });
 });
--- src/tests/ToggleField.test.jsx
+++ src/tests/ToggleField.test.jsx
@@ -1,24 +1,17 @@
 import ToggleField from "../components/ToggleField.jsx";
-import {render} from "@testing-library/react";
+import { render } from "@testing-library/react";
 
-test('render ToggleField component with default values', () => {
-    const {getByText} = render(
-        <ToggleField
-            description="Toggle"
-        />
-    );
-    expect(getByText('Toggle')).toBeInTheDocument();
+test("render ToggleField component with default values", () => {
+    const { getByText } = render(<ToggleField description="Toggle" />);
+    expect(getByText("Toggle")).toBeInTheDocument();
 });
 
-test('props test', () => {
+test("props test", () => {
     const description = "Test Description";
     const onChange = vi.fn();
 
-    const {getByText} = render(
-        <ToggleField
-            description={description}
-            onChange={onChange}
-        />
+    const { getByText } = render(
+        <ToggleField description={description} onChange={onChange} />,
     );
 
     expect(getByText(description)).toBeInTheDocument();
